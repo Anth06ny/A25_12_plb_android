f420f3c642811d6116cff39b7bf1f4fd08260e79
anth0 <anth06ny@gmail.com>
2025-12-11 10:21:51 +0100

    Correction TP Compose

--- app/src/main/java/com/amonteiro/a25_12_plb_android/presentation/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a25_12_plb_android/presentation/ui/screens/SearchScreen.kt
@@ -1,23 +1,35 @@
 package com.amonteiro.a25_12_plb_android.presentation.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.amonteiro.a25_12_plb_android.R
@@ -29,7 +41,7 @@ import com.amonteiro.a25_12_plb_android.presentation.viewmodel.MainViewModel
 @Preview(showBackground = true, showSystemUi = true)
 @Preview(
     showBackground = true, showSystemUi = true,
-    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES or android.content.res.Configuration.UI_MODE_TYPE_NORMAL
+    uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES or android.content.res.Configuration.UI_MODE_TYPE_NORMAL, locale = "fr"
 )
 @Composable
 fun SearchScreenPreview() {
@@ -43,22 +55,98 @@ fun SearchScreenPreview() {
 }
 
 @Composable
-fun SearchScreen(modifier: Modifier = Modifier,
-                 mainViewModel: MainViewModel = MainViewModel()) {
+fun SearchScreen(
+    modifier: Modifier = Modifier,
+    mainViewModel: MainViewModel = MainViewModel()
+) {
 
-    Column(modifier = modifier.fillMaxSize()) {
+    Column(modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        SearchBar()
+
 
         val list = mainViewModel.dataList.collectAsStateWithLifecycle().value
 
-        list.forEach {
-            PictureRowItem(data = it, modifier = Modifier.background(MaterialTheme.colorScheme.surfaceContainerHigh))
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)
+        , modifier = Modifier.weight(1f)
+        ) {
+            items(list.size) {
+                PictureRowItem(data = list[it])
+            }
+        }
+
+        Row {
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.bt_filter))
+            }
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Favorite,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.load_data))
+            }
+
+
         }
     }
 }
 
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "", //Valeur affichée
+        onValueChange = { newValue: String -> }, //Nouveau texte entrée
+        leadingIcon = { //Image d'icône
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        singleLine = true,
+        label = { //Texte d'aide qui se déplace
+            Text("Enter text")
+            //Pour aller le chercher dans string.xml, R de votre package com.nom.projet
+            //Text(stringResource(R.string.placeholder_search))
+        },
+        //placeholder = { //Texte d'aide qui disparait
+        //Text("Recherche")
+        //},
+
+        //keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search), // Définir le bouton "Entrée" comme action de recherche
+        //keyboardActions = KeyboardActions(onSearch = {onSearchAction()}), // Déclenche l'action définie
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
+}
+
+
 @Composable //Composable affichant 1 élément
 fun PictureRowItem(modifier: Modifier = Modifier, data: WeatherBean) {
-    Row(modifier = modifier.fillMaxWidth().padding(4.dp)) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .padding(4.dp)
+        .background(MaterialTheme.colorScheme.surfaceContainerHigh)) {
 
         //Permission Internet nécessaire
         AsyncImage(
@@ -82,12 +170,16 @@ fun PictureRowItem(modifier: Modifier = Modifier, data: WeatherBean) {
         )
 
         Column(modifier = Modifier.padding(4.dp)) {
-            Text(text = data.name,
+            Text(
+                text = data.name,
                 color = MaterialTheme.colorScheme.primary,
-                fontSize = 20.sp)
-            Text(text = data.getResume().take(20) + "...",
+                style = MaterialTheme.typography.titleMedium,
+            )
+            Text(
+                text = data.getResume().take(20) + "...",
                 color = MaterialTheme.colorScheme.onSurface,
-                fontSize = 14.sp)
+                style = MaterialTheme.typography.bodyLarge,
+            )
         }
     }
 }
